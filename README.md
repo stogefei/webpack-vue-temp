2c5c6b64bf046827eec8f206b46499b5a70ffad6
zhuaofei <zhuafei@authine.com>
2024-06-25 18:17:15 +0800

    feat: 🎸 readme调整

--- README.md
+++ README.md
@@ -1,21 +1,32 @@
 # 🚀 webpack打包一个vue组件
 
 ### 基于webpack5 vue2.7.14
+    typescript v4.9.5
+    webpack v5.0
+    vue v2.7
+    bpmn v14
 
 ### 支持ts tsx语法
+    支持 jsx语法 render函数 
 
 ### 启动
     pnpm i 
 
     yarn run build:dev
 
-### dome使用查看
+## 使用
+    引入组件
+    import CloudpivotBpmn from'../../../dist/index';
+    引入样式
+    @import '../../../dist/css/main.css';
+
+### dome 查看
     cd ./example
 
     pnpm i
 
     yarn run serve
 
-### 集成Bpmn组件
+### 最终效果
 
 ![alt text](image.png)
--- babel.config.js
+++ babel.config.js
@@ -16,3 +16,27 @@ module.exports = {
       
     ]
   }
+
+  // module.exports = {
+//   presets: [
+//     [
+//       "@babel/preset-env",
+//       {
+//       "corejs": {
+//           "version": 3,
+//           "proposals": true
+//         }
+//       }
+//     ],
+//     '@vue/app',
+//     [
+//       '@vue/babel-preset-jsx'
+//     ]
+//   ],
+//   plugins: [
+//     ['import', { libraryName: '@h3/antd-vue', libraryDirectory: 'lib', style: true }, '@h3/antd-vue'],
+//     [
+//       "@babel/plugin-transform-runtime",
+//     ],
+//   ]
+// };
